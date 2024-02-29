9a7fa495428bcaefd567154153003279fb5337fb
Daniel Bonasser <danielbonasser@gmail.com>
2024-02-29 18:52:16 -0300

    Add meals table to kenx type definition

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -7,5 +7,13 @@ declare module 'knex/types/tables' {
             name: string;
             email: string;
         };
+        meals: {
+            id: string;
+            name: string;
+            description: string;
+            created_at: string;
+            diet_meal: boolean;
+            user_id: string;
+        }
     }
 }
